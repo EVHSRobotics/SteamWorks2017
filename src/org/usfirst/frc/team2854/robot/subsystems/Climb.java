d71f23b5457593b6a10e8af1afe8b81cf90688ef
Rishi Parikh <rishiparikh2001@gmail.com>
2017-01-28 20:31:54 -0800

    subsystem created

---
+++ src/org/usfirst/frc/team2854/robot/subsystems/Climb.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team2854.robot.subsystems;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * @author Rishi
+ */
+public class Climb extends Subsystem{
+	private SpeedController f;
+	private SpeedController b;
+	public DriveTrain(SpeedController front,SpeedController back){
+		f = front;
+		b = back;
+	}
+
+	public void stop(){
+		f.set(0);
+		b.set(0);
+	}
+
+	public void run(double x){
+			if (Math.abs(math.pow(x, 3)) >= 0.1) {
+				f.set((yVal / 2));
+				b.set((yVal / 2));
+			} else {
+				f.set(0);
+				b.set(0);
+			}
+		}
+    public void initDefaultCommand(){
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
